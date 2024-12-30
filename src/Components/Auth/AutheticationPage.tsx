a01f332ce15169b40df88cb812019164b7fc5289
ShivaniMane <shivanimane43@gmail.com>
2024-12-30 21:35:28 +0530

    Design and implement authentication page UI

---
+++ src/Components/Auth/AutheticationPage.tsx
@@ -0,0 +1,12 @@
+import AuthenticationHeader from "./AuthenticationHeader"
+import LoginFrom from "./LoginFrom"
+const AutheticationPage = () => {
+  return (
+    <div className="bg-[#FCF5EB] h-[100vh]">
+    <AuthenticationHeader/>
+    <LoginFrom/>
+    </div>
+  )
+}
+ 
+export default AutheticationPage
