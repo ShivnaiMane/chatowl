685f563499e6c76e45cd344556865d6b0e4cb14e
ShivaniMane <shivanimane43@gmail.com>
2025-02-02 11:44:33 +0530

    feat: contaclist with chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
-import Authetication from "./Components/Auth/AutheticationPage"
+
+import ChatWindow from "./Components/ChatUI/ChatWindow";
 import ContactList from "./Components/Contact/ContactList";
 
 
 const  App:React.FC =() =>{
-  const [isLogin,setIsLogin] = useState<Boolean>(false)
-  if(isLogin){
-    return <Authetication/>
-  }
-
   return (
-<>
-  <div className="flex h-[100vh]">
+  <>
+  <div className="flex flex-col sm:flex-row h-[100vh]">
     <ContactList/>
-    <div className="flex flex-2"></div>
+    <ChatWindow/>
   </div>
-  <button onClick={()=>{setIsLogin(!isLogin)}}>Change</button>
   </>
   )
 }
